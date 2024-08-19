dcbb6549ecfedcc46e8037dbf2ed745a4ce5fc0b
AnonUser <127699732+w3irdguy@users.noreply.github.com>
2024-08-19 05:36:38 -0300

    Create todoscmds.sh

---
+++ todoscmds.sh
@@ -0,0 +1,24 @@
+# all commands in bash
+echo file
+echo stat
+echo pwd
+echo cd
+echo ls
+echo mv
+echo cp
+echo cat
+echo tac
+echo tail
+echo sed
+echo split
+echo cksum
+echo cmp
+echo touch
+echo rmdir
+echo mkdir
+echo rm
+echo chmod
+echo chown
+echo chgrp
+echo umask
+echo ln -s
